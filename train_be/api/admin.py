5cb7eccb928a29f19002914bb0ec4503f723013d
flond7 <elisa.utilivenza@gmail.com>
2022-04-28 09:58:29 +0200

    register tabs in admin

--- train_be/api/admin.py
+++ train_be/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Railway, Question, Video, Result
 
 # Register your models here.
+# it allows to have a user friendly interface to input data in the db
+
+admin.site.register(User)
+admin.site.register(Railway)
+admin.site.register(Video)
+admin.site.register(Question)
+admin.site.register(Result)
