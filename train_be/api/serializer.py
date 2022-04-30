8a01621a795a328e530fecb9b9b36c7f0fc59e84
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2022-04-30 21:38:28 +0200

    reordered serializer

--- train_be/api/serializer.py
+++ train_be/api/serializer.py
@@ -6,26 +6,28 @@ class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
     fields = '__all__'
-  
-class QuestionSerializer(serializers.ModelSerializer):
+
+class RailwaySerializer(serializers.ModelSerializer):
   class Meta:
     model = Question
-    fields = '__all__'  
+    fields = '__all__'
+    depth = 2
+
+class VideoSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Video
+    fields = '__all__'
+    depth = 1
+
+class QuestionSerializer(serializers.ModelSerializer):
+  id_video = VideoSerializer()
+
+  class Meta:
+    model = Question
+    fields = '__all__'
     depth = 2
 
 class ResultSerializer(serializers.ModelSerializer):
   class Meta:
     model = Result
     fields = '__all__'
-
-class VideoSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Video
-    fields = '__all__' 
-    depth = 1
-
-class RailwaySerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Question
-    fields = '__all__'  
-    depth = 2
