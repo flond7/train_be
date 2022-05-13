77e9b7308f11036a934da1da5795c24e68780ead
flond7 <elisa.utilivenza@gmail.com>
2022-05-13 09:02:14 +0200

    api refactoring

--- train_be/api/urls.py
+++ train_be/api/urls.py
@@ -2,11 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('video-detail/<int:pk>/', views.videoDetail.as_view(), name="video-detail"),
-  path('video-create/', views.videoCreate.as_view(), name="video-create"),
-  path('video-list/', views.videoList.as_view(), name="video-list"),
-  path('video-question-list/', views.videoQuestionlist.as_view(), name="video-question-list"),
+  path('question-detail/<int:pk>/', views.questionDetail.as_view(), name="question-detail"),
+  path('question-list/', views.questionList.as_view(), name="question-list"),
 
+  path('video-list/', views.videoList.as_view(), name="video-list"),
+  path('video-detail/<int:pk>/', views.videoDetail.as_view(), name="video-detail"),
+  path('video-question-list/<int:pk>', views.videoQuestionList, name="video-question-list"),
+
+  path('railway-list/', views.railwayList.as_view(), name="railway-list"),
+  path('railway-detail/<int:pk>/', views.railwayDetail.as_view(), name="railway-detail"),
+  path('railway-video-list/<int:pk>', views.railwayVideoList, name="railway-video-list"),
+
+  path('user-detail/<int:pk>/', views.userDetail.as_view(), name="user-detail"),
+  path('user-update/<int:pk>/', views.userUpdate.as_view(), name="user-update"),
+  path('user-results/<int:pk>', views.userResults, name="user-results"),
+
+  path('result-create/', views.resultCreate.as_view(), name="result-create"),
+  path('user-result/<int:pk>/', views.resultUpdate.as_view(), name="result-update"),
 ]
 
 """ urlpatterns = [
--- train_be/api/views.py
+++ train_be/api/views.py
@@ -1,14 +1,26 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view, permission_classes
+from django.http import JsonResponse
 
-from .models import Video, Question
-from .serializer import VideoSerializer
+from .serializer import UserSerializer, QuestionSerializer, ResultSerializer, VideoSerializer, RailwaySerializer
+from .models import User, Question, Result, Video, Railway
 
+
+
+
+class questionList(generics.ListAPIView):
+  queryset = Question.objects.all()
+  serializer_class = QuestionSerializer
+
+class questionDetail(generics.RetrieveAPIView):
+  queryset = Question.objects.all()
+  serializer_class = QuestionSerializer
+
+
+
+###### VIDEO
 
 class videoDetail(generics.RetrieveAPIView):
-  queryset = Video.objects.all()
-  serializer_class = VideoSerializer
-
-class videoCreate(generics.CreateAPIView):
   queryset = Video.objects.all()
   serializer_class = VideoSerializer
 
@@ -16,19 +28,82 @@ class videoList(generics.ListAPIView):
   queryset = Video.objects.all()
   serializer_class = VideoSerializer
 
-""" send the object with the key: values you want to change """
-class videoUpdate(generics.UpdateAPIView):
-  queryset = Video.objects.all()
-  serializer_class = VideoSerializer
-  lookup_field = 'pk'
-
-  def perform_update(self, serializer):
-    instance = serializer.save()
-    return super().perform_update(serializer)
-
-class videoQuestionlist(generics.ListAPIView):
-  lookup_field = 'pk'
+@api_view(['GET'])
+def videoQuestionList(request, pk):
   queryset = Question.objects.prefetch_related('video').filter(video=pk)
   res = []
   for q in queryset:
     res.append({'question-id': q.id, 'video-id': q.video.id,'text': q.text, 'answerOne': q.answerOne, 'answerTwo': q.answerTwo, 'answerThree': q.answerThree, 'correct': q.correct})
+  return JsonResponse(res, safe=False)
+
+
+
+###### RAILWAYS
+
+class railwayList(generics.ListAPIView):
+  queryset = Railway.objects.all()
+  serializer_class = RailwaySerializer
+
+class railwayDetail(generics.RetrieveAPIView):
+  queryset = Railway.objects.all()
+  serializer_class = RailwaySerializer
+
+@api_view(['GET'])
+def railwayVideoList(request, pk):
+  queryset = Video.objects.prefetch_related('railway').filter(railway=pk)
+  res = []
+  for q in queryset:
+    res.append({'railway-id': q.railway.id, 'video-id': q.id, 'description': q.description, 'url': q.url})
+  return JsonResponse(res, safe=False)
+
+
+
+###### USER
+
+class userDetail(generics.RetrieveAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+class userUpdate(generics.UpdateAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+  lookup_field = 'pk'
+
+@api_view(['GET'])
+def userResults(request, pk):
+  queryset = Result.objects.prefetch_related('user').filter(user=pk)
+  res = []
+  for q in queryset:
+    res.append({
+      'resultId': q.id,
+      'resultCorrect': q.correct,
+      'questionId':q.question.id,
+      'questionText': q.question.text,
+      'videoId': q.question.video.id,
+      'videoName': q.question.video.name,
+      'railwayId': q.question.video.railway.id,
+      'railwayName': q.question.video.railway.name,
+      'userId': q.user.id})
+  return JsonResponse(res, safe=False)
+
+
+
+###### RESULT
+
+class resultCreate(generics.CreateAPIView):
+  queryset = Result.objects.all()
+  serializer_class = ResultSerializer
+
+  """ HOOK TO ADD IMPLICIT INFORMATION that are not sent with the data by the user
+  def perform_create(self, serializer):
+    serializer.save(user=self.request.user) """
+
+""" send the object with the key: values you want to change """
+class resultUpdate(generics.UpdateAPIView):
+  queryset = Result.objects.all()
+  serializer_class = ResultSerializer
+  lookup_field = 'pk'
+
+"""  HOOK TO ADD ACTIONS, such us sending a confirmation email to the user
+  def perform_update(self, serializer):
+    instance = serializer.save() """
