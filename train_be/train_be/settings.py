7ba92f2527e8bbbf5e2fda36b6b0e5f5d5902fcf
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2022-06-12 11:46:27 +0200

    cleaning

--- train_be/train_be/settings.py
+++ train_be/train_be/settings.py
@@ -130,8 +130,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         #'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
 }
